a0c31d183c1fe3199afce535bef99f83149fe37d
Noname191022 <lucasmesquitarage92@outlook.com>
2026-02-03 19:59:16 -0400

    finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+System.out.println("---Balanço Trimestral---");
+float gastosJaneiro = 15000.00f;
+float gastosFevereiro = 23000.00f;
+float gastosMarco = 17000.00f;
 
+float gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+System.out.println("Gasto deste Trimestre foi: R$" + gastosTrimestre );
         }
     }
-}
